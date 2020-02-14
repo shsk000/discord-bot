dc5da4f9b5eb91516794d4f656131e15b8664cc7
fatbros <02luvsic@gmail.com>
2020-02-15 05:48:15 +0900

    add: play stream

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-import Discord from "discord.js";
+import { createReadStream } from "fs";
+import path from "path";
+import Discord, { VoiceChannel } from "discord.js";
+// import ytdl from "ytdl-core";
 
 declare const process: {
   env: {
@@ -18,5 +21,41 @@ client.on("ready", () => {
 
 const onMessage = new OnMessage(client);
 onMessage.triggerEventListener();
+
+client.on("voiceStateUpdate", async (oldMember, newMember) => {
+  if (oldMember.user.bot || newMember.user.bot) return;
+
+  // tyu
+  // const targetUserId = "272883674842660864";
+  // mo
+  // const targetUserId2 = "351362773407498241";
+
+  const channelId = newMember.voiceChannelID;
+  // const joinedUserId = oldMember.user.username;
+  // if (targetUserId !== joinedUserId) return;
+
+  const channel = client.channels.get(channelId);
+
+  if (!channel) return;
+  if (!(channel instanceof VoiceChannel)) return;
+
+  try {
+    const connection = await channel.join();
+
+    const filepath = path.join(__dirname, "../src/data/ksk.mp3");
+    const stream = createReadStream(filepath, {
+      autoClose: true
+    });
+    const dispatcher = connection.playStream(stream);
+
+    dispatcher.on("end", () => {
+      stream.destroy();
+      channel.leave();
+      connection.disconnect();
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 client.login(process.env.DISCORD_TOKEN);
