f2adecfc6b83081159897723b4a6dd9f06f17067
Shun Kondo <02luvsic@gmail.com>
2021-07-14 00:19:29 +0900

    add process debug log

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ const client = new Discord.Client();
 
 client.on("ready", async () => {
   logger.debug("ready...");
+  logger.info(process.env);
 
   const channel = await client.channels.fetch(process.env.DEBUG_CHANNEL_ID);
 
